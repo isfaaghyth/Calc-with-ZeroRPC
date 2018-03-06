8c324ba237df63c4c3b7162157256ec9e4876555
Muh Isfhani Ghiath <isfaaghyth@gmail.com>
2018-03-06 16:55:29 +0700

    nambahin update.sh @ Sel Mar  6 16:55:29 WIB 2018

--- calc2.py
+++ calc2.py
@@ -1,7 +1,2 @@
-n1 = input("Masukan angka pertama = ");
-n2 = input("Masukan angka kedua =");
-print "Hasil Dari Pertambahan",tambah(n1,n2)
-
 def tambah(a,b):
-	tambah = (a + b)
-	return tambah
+	return (a+b)
---
+++ update.sh
@@ -0,0 +1,3 @@
+git add -A
+git commit -m "$1 @ $(date)"
+git push origin master
